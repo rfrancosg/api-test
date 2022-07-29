590e9657567d357840dcec1ca804680b4f3da277
rfrancosg <61679511+rfrancosg@users.noreply.github.com>
2022-07-29 13:58:03 +0200

    fix to execute

--- src/main/java/com/monkeys/api_test/models/entities/User.java
+++ src/main/java/com/monkeys/api_test/models/entities/User.java
@@ -1,4 +1,18 @@
 package com.monkeys.api_test.models.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
--- src/main/java/com/monkeys/api_test/services/implementations/UserServiceImpl.java
+++ src/main/java/com/monkeys/api_test/services/implementations/UserServiceImpl.java
@@ -1,15 +1,10 @@
 package com.monkeys.api_test.services.implementations;
 
-import com.monkeys.api_test.repositories.UserRepository;
 import com.monkeys.api_test.services.interfaces.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-
-    @Autowired
-    UserRepository userRepository;
 
 
 }
